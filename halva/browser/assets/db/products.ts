eccfc4f7c63e752ce03a78227067951a13f0a6b8
Anvar <fort2652@rambler.ru>
2023-12-31 19:28:07 +0300

    add product

--- halva/browser/assets/db/products.ts
+++ halva/browser/assets/db/products.ts
@@ -1,6 +1,6 @@
 const  products = [{
 id: Date.now().toString(16),
-title: 'Меринговый рулет',
+title: 'Меренговый рулет',
 description: 'Нежный и воздушный, словно сделан из облаков. Легкая кислинка в начинке прекрасно оттеняет сладость меренги. Это совсем не похоже на безе.',
 price: 700,
 weight: 1200,
@@ -11,23 +11,23 @@ nutritions:
     fats: 26,
     carbs: 80
   },
-img: '../assets/images/mering.jpg'
+img: '/assets/images/mereng.jpg'
 
 },
 {
   id: Date.now().toString(16),
-  title: 'Меринговый рулет',
-  description: 'Нежный и воздушный, словно сделан из облаков. Легкая кислинка в начинке прекрасно оттеняет сладость меренги. Это совсем не похоже на безе.',
-  price: 700,
-  weight: 1200,
+  title: 'Три шоколада',
+  description: 'Невероятно вкусный торт! Нежный, с ярко выраженным вкусом шоколада — оценят и взрослые, и дети. А уж про шоколадоголиков вообще молчу )',
+  price: 1300,
+  weight: 1500,
   nutritions: 
     {
-      kKal: 579,
-      proteins: 7,
-      fats: 26,
-      carbs: 80
+      kKal: 988,
+      proteins: 16,
+      fats: 68,
+      carbs: 79
     },
-  img: '../assets/images/mering.jpg'
+  img: '/assets/images/tri-shokolada.jpg'
   
   }
 ]
