834581fa2b84ae20dabd32b002e46cf3777f0b0e
Anvar <fort2652@rambler.ru>
2024-01-01 21:51:57 +0300

    start carousel

--- src/app/core/components/carousel/carousel.component.ts
+++ src/app/core/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './carousel.component.scss'
 })
 export class CarouselComponent {
+@Input() images = []
+currentImageIndex = 0
 
+nextSlide() {
+  if(this.currentImageIndex < this.images.length)
+  this.currentImageIndex++
+}
+prevSlide() {
+  if(this.currentImageIndex > 0)
+  this.currentImageIndex--
+}
 }
