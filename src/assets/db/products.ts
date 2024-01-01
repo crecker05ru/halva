80456333e36f77d23b1b487141e057b08ae343b0
Anvar <fort2652@rambler.ru>
2024-01-01 21:22:38 +0300

    add prod

--- src/assets/db/products.ts
+++ src/assets/db/products.ts
@@ -1,4 +1,6 @@
-const  products = [{
+import { Product } from "../../app/shared/product"
+
+const  products:Product[] = [{
 id: Date.now().toString(16),
 title: 'Меренговый рулет',
 description: 'Нежный и воздушный, словно сделан из облаков. Легкая кислинка в начинке прекрасно оттеняет сладость меренги. Это совсем не похоже на безе.',
@@ -15,7 +17,7 @@ img: './assets/images/mereng.jpg'
 
 },
 {
-  id: Date.now().toString(16),
+  id: (Date.now() - 2).toString(16),
   title: 'Три шоколада',
   description: 'Невероятно вкусный торт! Нежный, с ярко выраженным вкусом шоколада — оценят и взрослые, и дети. А уж про шоколадоголиков вообще молчу )',
   price: 1300,
@@ -27,9 +29,64 @@ img: './assets/images/mereng.jpg'
       fats: 68,
       carbs: 79
     },
-  img: 'assets/images/tri-shokolada.jpg'
-  
-  }
+  img: './assets/images/tri-shokolada.jpg'
+  },{
+    id: (Date.now() - 3).toString(16),
+    title: 'Торт Красный бархат',
+    description: 'Красные бархатные коржи с творожным кремом играют ванильно-молочными нотками сладкого послевкусия.',
+    price: 1300,
+    weight: 1500,
+    nutritions: 
+      {
+        kKal: 988,
+        proteins: 16,
+        fats: 68,
+        carbs: 79
+      },
+    img: './assets/images/krasniy-barhat.JPG'
+    },{
+      id: (Date.now() - 4).toString(16),
+      title: 'Медовик',
+      description: 'Воздушные коржи со сладким ароматом цветочного меда и нежный сливочно-сметанный крем с добавлением вареной сгущенки.Оформлен медово-бисквитной крошкой и декоративной сахарной пудрой и декоративной сахарной пудрой',
+      price: 900,
+      weight: 1500,
+      nutritions: 
+        {
+          kKal: 988,
+          proteins: 16,
+          fats: 68,
+          carbs: 79
+        },
+      img: './assets/images/medovik.JPG'
+      },{
+        id: (Date.now() - 5).toString(16),
+        title: 'Венский пирог',
+        description: '',
+        price: 800,
+        weight: 1500,
+        nutritions: 
+          {
+            kKal: 988,
+            proteins: 16,
+            fats: 68,
+            carbs: 79
+          },
+        img: './assets/images/venski-pirog.JPG'
+        },{
+          id: (Date.now() - 6).toString(16),
+          title: 'Курагавая каша',
+          description: '',
+          price: 300,
+          weight: 1500,
+          nutritions: 
+            {
+              kKal: 988,
+              proteins: 16,
+              fats: 68,
+              carbs: 79
+            },
+          img: './assets/images/kuragovaya-casha.HEIC'
+          }
 ]
 
 export default products
